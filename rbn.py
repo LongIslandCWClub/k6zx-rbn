68588ecbc2eeefbc7a0daf0e2beddf2969fab0bc
Steve Witt <sawitt@ieee.org>
2019-05-23 14:07:54 -0700

    more progress

--- rbn.py
+++ rbn.py
@@ -2,6 +2,7 @@
 
 
 import configargparse
+import logging
 import os
 import re
 import signal
@@ -49,16 +50,18 @@ def parseArguments():
 
     parser.add_argument('-b', '--band', action='append', dest='band',
                         help='Display stations only on these bands')
-    parser.add_argument('-d', '--debug', action='store', dest='debug',
-                        type=int, default = 0, help='DE ITU Zone')
+    parser.add_argument('-l', '--logging', action='store', dest='logging',
+                        type=int, default = 0, help='Enable program logging')
+    parser.add_argument('--telnetdebug', action='store', dest='telnetdebug',
+                        type=int, default = 0, help='Enable telnetlib debugging')
     parser.add_argument('--de_itu', action='store', dest='deITU',
                         type=int, help='DE ITU Zone')
     parser.add_argument('--dx_itu', action='store', dest='dxITU',
                         type=int, help='DX ITU Zone')
     parser.add_argument('--de_cq', action='append', dest='deCQ',
-                        type=int, help='DE CQ Zone')
+                        help='DE CQ Zone')
     parser.add_argument('--dx_cq', action='append', dest='dxCQ',
-                        type=int, help='DX CQ Zone')
+                        help='DX CQ Zone')
     parser.add_argument('--min_wpm', action='store', dest='minWPM',
                         type=int, default=0, help='Minimum CW WPM to show')
     parser.add_argument('--max_wpm', action='store', dest='maxWPM',
@@ -104,7 +107,8 @@ def processArgs(args):
         a['mode'] = ['CW', 'RTTY', 'PSK31', 'PSK63', 'BPSK', 'FT8', 'FT4']
     else:
         a['mode'] = args.mode
-    a['debug'] = args.debug
+    a['logging'] = args.logging
+    a['telnetdebug'] = args.telnetdebug
     
     return a
 
@@ -148,6 +152,9 @@ def filterBand(args, freq):
         if 50000 <= freq <= 54000:
             result = True
 
+    if args['logging'] and result == False:
+        logging.warning("filterBand(): cfg {args['band']}, {freq}")
+        
     return result
 
 
@@ -172,6 +179,9 @@ def filterMode(args, mode):
         result = True
     if 'FT4' in mode:
         result = True
+
+    if args['logging'] and result == False:
+        logging.warning(f"filterMode(): cfg {args['mode']}, mode {mode}")
         
     return result
 
@@ -185,6 +195,10 @@ def filterWPM(args, wpmStr):
     if args['minWPM'] <= wpm <= args['maxWPM']:
         result = True
     
+    if args['logging'] and result == False:
+        logging.warning(f"filterWPM(): min {args['minWPM']}, max {args['maxWPM']} "
+                        f"WPM {wpm}")
+        
     return result
 
 
@@ -195,9 +209,27 @@ def filterCQZones(args, callData):
         # print(f"filterCQZones() {args['dxCQ']}")
         # print(f"\tcq zone: {callData['cqzone']}")
 
-        if callData['cqzone'] in args['dxCQ']:
+        # need to convert from string to int to match zone types in the
+        # progArgs dxCQ list
+        zone = int(callData['cqzone'])
+        
+        logging.warning(f"calldata cqzone type {type(callData['cqzone'])}, "
+                        f"args dxCQ type {type(args['dxCQ'])}, "
+                        f"args elem type {type(args['dxCQ'][0])}")
+        
+        if zone in args['dxCQ']:
             result = True
-
+        else:
+            if args['logging']:
+                logging.warning(f"filterCQZones(): cfg CQ zone {args['dxCQ']} "
+                                f"CQ zone {zone}")
+            else:
+                pass
+    else:
+        result = True          # this call doesn't have a CQ Zone so print it
+        if args['logging']:
+            logging.warning(f"filterCQZones(): station has no 'cqzone'")
+        
     return result
 
 
@@ -208,8 +240,9 @@ def filter(progArgs, qrz, line):
     l = lineStr.split()
 
     if len(l) > 7:
-        # print("-------------------------------")
-        # print(f"split: {l}")
+        if progArgs['logging']:
+            logging.warning("-------------------------------")
+            logging.warning(f"split: {l}")
 
         deCall = l[2].split('-')[0]
         freq = float(l[3])
@@ -221,8 +254,9 @@ def filter(progArgs, qrz, line):
         # time = l[11]
         time = l[(len(l) - 1)]
 
-        # print(f"DEBUG: {dxCall} de {deCall}, freq {freq}, {mode}, {snr} dB, "
-        #       f"{wpm} WPM, {time}Z")
+        if progArgs['logging']:
+            logging.warning(f"DEBUG: {dxCall} de {deCall}, freq {freq}, {mode}, "
+                            f"{snr} dB, {wpm} WPM, {time}Z")
 
         try:
             dxCallData = qrz.callsignData(dxCall, quiet=True)
@@ -252,10 +286,6 @@ def filter(progArgs, qrz, line):
 def filter1(progArgs, qrz, line):
     lineStr = line.decode('utf-8').rstrip()
 
-    # patternStr = (f"DX\s+de\s+([a-zA-Z0-9]+)-#:\s+([0-9.]+)\s+"
-    #               "([a-zA-Z0-9/]+)\s+([a-zA-Z]+)\s+([0-9]+)\s+dB\s+"
-    #               "([0-9]+)\s+WPM|BPS\s+[a-zA-Z0-9\s]+\s+([0-9]+)Z")
-    # pattern = re.compile(patternStr)
     pattern = re.compile(r"""
     DX\s+de\s+
     ([a-zA-Z0-9/\-\/]+)-\#:\s+                 # receiving (de) station
@@ -317,13 +347,6 @@ def filter1(progArgs, qrz, line):
     return retStr
 
 
-# def spinningCursor():
-#     while True:
-#         for cursor in '|/-\\':
-#             yield cursor
-
-    
-
 def main():
     global telnetInstance
 
@@ -332,11 +355,24 @@ def main():
     progArgs = processArgs(args)
     print(f"DEBUG - progArgs: {progArgs}")
 
-    tn = telnetlib.Telnet(RBN_HOST, RBN_PORT)
+    if progArgs['logging']:
+        logging.basicConfig(filename='rbn.log', filemode='w')
+
+    # tn = telnetlib.Telnet(RBN_HOST, RBN_PORT)
+    tn = telnetlib.Telnet()
     telnetInstance = tn
 
-    tn.set_debuglevel(progArgs['debug'])
+    tn.set_debuglevel(progArgs['telnetdebug'])
 
+    for x in range(10):
+        try:
+            tn.open(RBN_HOST, RBN_PORT, 10)
+            print(f"telnet connection established {RBN_HOST}:"
+                  f"{RBN_PORT}")
+            break
+        except socket.timeout:
+            print(f"telnet connection timed out, retrying...")
+            
     tn.read_until(b"Please enter your call: ")
     loginStr = (MY_CALLSIGN + "\n").encode('ascii')
     tn.write(loginStr)
